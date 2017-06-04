bc9f9f1756631e4146c7ad95de0714f58e527659
Kengo <kengo971114@gmail.com>
2017-06-04 20:36:06 +0900

    jqueryサンプル作成

--- gulpfile.js
+++ gulpfile.js
@@ -12,66 +12,67 @@ var uglify = require('gulp-uglify');
 var rimraf = require('rimraf');
 var runsequence = require('run-sequence');
 
-
-gulp.task('sass',function(){
+// SassファイルをCSSにコンパイル
+gulp.task('sass', function() {
+  // タスクを定義する
   gulp.src('src/assets/sass/*.scss')
-  .pipe(plumber({errorHandler: notify.onError('<%- error.message %>')}))
+  // エラー通知
+  .pipe(plumber({errorHandler: notify.onError('<%- error.mesagge %>')}))
+  // タスクを実行するファイルを指定
   .pipe(sass())
+  // 実行する処理
   .pipe(autoprefixer())
+  // ベンダープレフィックス付与
   .pipe(cssmin())
+  // css圧縮
   .pipe(rename({suffix: '.min'}))
+  // minにリネーム
   .pipe(gulp.dest('dest/assets/css'));
+  // 出力先の指定
 });
 
-gulp.task('js',function(){
+// JSファイルの圧縮
+gulp.task('js', function() {
   gulp.src('src/assets/js/**/*.js')
-  .pipe(plumber({errorHandler: notify.onError('<%- error.message %>')}))
+  .pipe(plumber({errorHandler: notify.onError('<%- error.mesagge %>')}))
   .pipe(uglify())
   .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('dest/assets/js'));
 });
 
-gulp.task('html',function(){
+// HTMLファイルをdestに出力する
+gulp.task('html', function() {
   gulp.src('src/**/*.html')
   .pipe(gulp.dest('dest/'));
 });
 
-gulp.task('copy',function(){
+// 静的ファイルをコピー
+gulp.task('copy', function() {
   gulp.src('src/assets/fonts/**/*')
-  .pipe(gulp.dest('dest/assets/fonts'));
-  gulp.src('srcc/assets/img/**/*')
-  .pipe(gulp.dest('dest/assets/img'));
+    .pipe(gulp.dest('dest/assets/fonts'));
+  gulp.src('src/assets/img/**/*')
+    .pipe(gulp.dest('dest/assets/img'));
   gulp.src('src/assets/data/**/*')
-  .pipe(gulp.dest('dest/assets/data'));
+    .pipe(gulp.dest('dest/assets/data'));
 });
 
-gulp.task('clean',function(cb){
+// 出力先のdestを一度空にする
+gulp.task('clean', function(cb) {
   return rimraf('dest', cb);
 });
 
-gulp.task('build', ['clean'],function(callback){
+// gulp buildで様々なタスクを実行させる
+gulp.task('build', ['clean'], function(callback) {
   return runsequence(
     'copy',
-    ['html','sass','js'],
+    ['html', 'sass', 'js'],
     callback
   );
 });
 
-// gulp.task('reload', function(){
-//   browserSync.reload();
-// });
 gulp.task('reload', function() {
   browserSync.reload();
 });
-
-// gulp.task('default', function(){
-//   browserSync({
-//     server: {baseDir: 'dest'}
-//   });
-//   gulp.watch('src/**/*.html', ['reload']);
-//   gulp.watch('src/**/*.html', ['html']);
-//   gulp.watch('src/assets/sass/*.sass', ['sass']);
-// });
 
 // watchするタスク
 gulp.task('watch', function() {
