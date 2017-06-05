054581a07b0e461bf3e1a888e09dda5c0f5524b2
Kengo <kengo971114@gmail.com>
2017-06-06 00:01:05 +0900

    制作過程における保存２

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,7 @@ $(function(){
   $('a img').hover(function(){
     $(this).attr('src',$(this).attr('src').replace('_off','_on'));
   },function(){
-    if (!$(this).hasClass('currentPage')){
+    if (!$(this).hasClass('currentPage')) {
       $(this).attr('src',$(this).attr('src').replace('_on','_off'));
     }
   });
