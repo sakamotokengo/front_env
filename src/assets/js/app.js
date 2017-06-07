2f519c44eb6517832dd8f2c43d9dac18d1227840
Kengo <kengo971114@gmail.com>
2017-06-07 19:58:44 +0900

    practice15完成！完成！

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,5 @@
 $(function(){
-  $('.checkbox').change(function(){
-    if($(this).is(':checked')){
-      $(this).next('label').addClass('LabelSelected');
-    }else{
-      $(this).next('label').removeClass('LabelSelected');
-    }
-  });
-  $('.radio').change(function(){
-    if($(this).is(':checked')){
-
-      $('.RadioSelected:not(:checked)').removeClass('RadioSelected');
-      $(this).next('label').addClass('RadioSelected');
-    }
+  $('.open').click(function(){
+    $('#slideBox').slideToggle('slow');
   });
 });
