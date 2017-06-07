e0ce39a281973c0473c6a96215e2d27343bf4a97
Kengo <kengo971114@gmail.com>
2017-06-07 17:46:40 +0900

    practice11完成

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,8 @@
 $(function(){
-  $('form:input').focus(function(){
+  $('form :input').focus(function() {
     $('label[for="' + this.id + '"]').addClass('labelfocus');
   });
+  
   $('form :input').blur(function(){
     $("label[for='" + this.id + "']").removeClass('labelfocus');
   });
