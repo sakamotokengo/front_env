d8ea9c21823550872abaab7c479caa76afa85a6c
Kengo <kengo971114@gmail.com>
2017-06-07 11:09:59 +0900

    practice06完成！

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,5 @@
 $(function(){
-  $('.toTop').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    },1600);
-    return false;
+  $('img').error(function(){
+    $(this).attr({src:'http://webcreatorbox.com/sample/images/missing.jpg',alt:'画像が見つかりません'});
   });
 });
