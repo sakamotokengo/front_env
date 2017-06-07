f558e086658483bed5e04d9d112554e7a2be57e1
Kengo <kengo971114@gmail.com>
2017-06-07 17:13:31 +0900

    practice11途中過程１

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,8 @@
 $(function(){
-  $('.deleteBox .delete').click(function(){
-    $(this).parents('deleteBox').animate({opacity: 'hide'}, 'slow');
+  $('form:input').focus(function(){
+    $('label[for="' + this.id + '"]').addClass('labelfocus');
+  });
+  $('form :input').blur(function(){
+    $("label[for='" + this.id + "']").removeClass('labelfocus');
   });
 });
